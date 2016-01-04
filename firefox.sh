d8371dceb3fb27c75584cee21ac122492584b218
Samer Masterson <samer@samertm.com>
2016-01-03 20:28:57 -0800

    add firefox.sh

---
+++ firefox.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+cat <<EOF
+Install these addons:
+https://addons.mozilla.org/en-us/firefox/addon/adwaita/
+https://addons.mozilla.org/en-us/firefox/addon/htitle/
+https://addons.mozilla.org/en-US/firefox/addon/disable-ctrl-q-shortcut/
+EOF
