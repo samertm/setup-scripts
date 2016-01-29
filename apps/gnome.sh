ca338d281d6c85b0401cb0971afd82f7cedabe88
Samer Masterson <samer@samertm.com>
2016-01-29 14:18:53 -0800

    fjkdls

--- apps/gnome.sh
+++ apps/gnome.sh
@@ -7,3 +7,6 @@ sudo apt-get install ubuntu-gnome-desktop
 
 # Turn off bell.
 gsettings set org.gnome.desktop.wm.preferences audible-bell false
+
+# Set capslock to ctrl.
+echo "Open the Gnome Tweak Tool. Select Typing > Ctrl key position > Caps Lock as Ctrl."
