8bc1dc918e051db0dfaa3dd63f6e10390efd483d
Samer Masterson <samer@samertm.com>
2016-02-14 18:07:07 -0800

    stuff

--- init/01-directories.sh
+++ init/01-directories.sh
@@ -4,4 +4,5 @@ IFS=$'\n\t'
 
 mkdir $HOME/src && true
 mkdir $HOME/tmp && true
+mkdir $HOME/bin && true
 ln -s $HOME/Dropbox/org $HOME/org && true
--- tools/apt-tools.sh
+++ tools/apt-tools.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 IFS=$'\n\t'
 
 . ../common/require.sh
-apt_require silversearcher-ag tree
+apt_require silversearcher-ag tree python-pip python-dev libffi-dev libssl-dev gimp
