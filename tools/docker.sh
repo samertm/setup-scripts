6ea08b0d6d31b3a9902669710747201c9e31350d
Samer Masterson <samer@samertm.com>
2016-02-18 23:38:26 -0800

    docker

--- tools/docker.sh
+++ tools/docker.sh
@@ -4,3 +4,5 @@ IFS=$'\n\t'
 
 curl -sSL https://get.docker.com/ | sh
 sudo usermod -aG docker samer
+
+echo 'follow these instructions: http://container-solutions.com/set-the-ip-of-the-docker-bridge-with-systemd/'
